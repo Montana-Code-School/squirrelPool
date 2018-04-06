de564083682f0426d8f70a0193f32fc4c002dfb5
Tim Fox <35637638+Timfoxtimfox@users.noreply.github.com>
2018-04-06 13:18:54 -0600

    tryina switch branches

--- App.test.js
+++ App.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import App from './App';
-
+import Adapter from 'enzyme-adapter-react-16';
+import Enzyme, { shallow, configure } from 'enzyme';
 import renderer from 'react-test-renderer';
 
 it('renders without crashing', () => {
   const rendered = renderer.create(<App />).toJSON();
   expect(rendered).toBeTruthy();
+});
+it('has a button on the front page', () => {
+  const rendered = renderer.create(<App />).toJSON();
+  expect(rendered.children[3].props.accessibilityComponentType === 'button').toBe(true);
 });
