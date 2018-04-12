57268c36cdadde208fc40e8cd60779b06a70a944
Tim Fox <35637638+Timfoxtimfox@users.noreply.github.com>
2018-04-12 16:48:14 -0600

    added new tile layer, extended test coverage

--- __TEST__/App.test.js
+++ __TEST__/App.test.js
@@ -2,77 +2,78 @@ import React from 'react';
 import App from '../App';
 import LandingPage from '../Component/Landingpage.js'
 import GamePage from '../Component/Gamepage.js'
-import Enzyme, { shallow, mount, configure } from 'enzyme';
-import { TouchableHighlight, Alert, Image, StyleSheet, Text, View, NavigatorIOS } from 'react-native';
+import Enzyme,
+  {
+    shallow,
+    mount,
+    configure
+  } from 'enzyme';
+import {
+  TouchableHighlight,
+  Alert,
+  Image,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 import Adapter from 'enzyme-adapter-react-16';
 import renderer from 'react-test-renderer';
 import reactDOM from 'react-dom';
 import sinon from 'sinon';
+import { StackNavigator } from 'react-navigation';
 
 Enzyme.configure({ adapter: new Adapter() })
-// const onTestPress = sinon.spy();
-// const onNavigate = sinon.spy();
-// const landShallow = shallow(<LandingPage navigator={{push:onNavigate}} onPress={onTestPress } />);
-//const gameShallow = shallow(<GamePage />);
 const rendered = renderer.create(<App />).toJSON();
+const renderedLanding = renderer.create(<LandingPage />).toJSON();
+const mockNav = {navigate:sinon.spy()};
+const shallowLanding = shallow(<LandingPage navigation={mockNav} />);
+const renderedGame = renderer.create(<GamePage />).toJSON();
 
 describe('these are our tests. there are many like them, but these ones are ours.', () => {
-// console.log("FULL FILE CONTENTS");
-// console.log("vvvvv rendered vvvvv");
-// console.log(rendered);
-// console.log("vvv Rendered type vvvv");
-// console.log(rendered.type);
-// console.log("vvvvv rendered props vvvv");
-// console.log(rendered.props);
-// console.log("vvvvvv rendered.children vvvvv");
-// console.log(rendered.children);
-// console.log("RENDERED.CHILDREN CONTENTS");
-// console.log(rendered.children[0].type);
-// console.log("vvvvv rendered children[0].props vvvvvv");
-// console.log(rendered.children[0].props);
-// console.log("vvvvv rendered.children[0].children vvvvvvvv");
-// console.log(rendered.children[0].children);
-// console.log("vvvvv rendered.children[0].children[0] vvvvvvvv");
-// console.log(rendered.children[0].children[0]);
-// console.log("vvvvv rendered.children[0].children[0].children vvvvvvvv");
-// console.log(rendered.children[0].children[0].children);
-// console.log(rendered.children[0].children[0].children[0].children);
-// console.log(rendered.children[0].children[0].children[0].children[0]);
-// console.log(rendered.children[0].children[0].children[0].children[0].children);
-// console.log(rendered.children[0].children[0].children[0].children[0].children[0]);
-// console.log(rendered.children[0].children[0].children[0].children[0].children[0].children);
-// console.log(rendered.children[0].children[0].children[0].children[0].children[0].children[0].children);
+  describe('LandingPage Test, rendered through App', () => {
 
-  it('renders without crashing', () => {
-  expect(rendered).toBeTruthy();
-  });
+    it('renders without crashing', () => {
+      expect(rendered).toBeTruthy();
+    });
 
-  it('has four elements; text(landingpirate), image, text(secondtext), touchablehighlight', () => {
-    expect(
-      rendered
-      .children[0]
-      .children[0]
-      .children[0]
-      .children[0]
-      .children[0]
-      .children
-      .length
-    )
-    .toBe(4)
-  })
+    it('has four elements; text(landingpirate), image, text(secondtext), touchablehighlight', () => {
+      expect(
+        rendered
+        .children[0]
+        .children[0]
+        .children[0]
+        .children[0]
+        .children[0]
+        .children
+        .length
+      )
+      .toBe(4)
+    })
 
-  it('child [0] is text(landingpirate)', () => {
-    expect(
-      rendered
-      .children[0]
-      .children[0]
-      .children[0]
-      .children[0]
-      .children[0]
-      .children[0]
-      .children[0]
-    )
-    .toBe('TEXT FROM LANDING Pirate')
-  })
+    it('child [0] is text(landingpirate)', () => {
+      expect(
+        rendered
+        .children[0]
+        .children[0]
+        .children[0]
+        .children[0]
+        .children[0]
+        .children[0]
+        .children[0]
+      )
+      .toBe('TEXT FROM LANDING Pirate')
+    })
+
+    it('gamePageNav works onPress', () => {
+      expect(shallowLanding.find('TouchableHighlight').simulate('press'));
+    })
 
   })
+
+  describe('GamePage Test', () => {
+
+    it('renders with out crashing', () => {
+      expect(renderedGame).toBeTruthy();
+    })
+  })
+})
