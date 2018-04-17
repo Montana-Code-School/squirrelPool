303e4621e0003eaadef88092f39d25dcc9346fe6
Eli Dery <elidery@Ye-Olde-Macbook-Pro.local>
2018-04-17 16:38:33 -0600

    Working ship navigation. NExt is collision

--- Component/GameAssets/Sprite.js
+++ Component/GameAssets/Sprite.js
@@ -6,15 +6,6 @@ import { Sprite, Body, Loop } from 'react-game-kit/native';
 import RedShip1 from '../Assets/Images/Sprites/RedShip1.png';
 
 export default class ShipSprite extends Component {
-  static propTypes = {
-    keys: PropTypes.object,
-    store: PropTypes.object,
-  };
-
-  static contextTypes = {
-    engine: PropTypes.object,
-    scale: PropTypes.number,
-  };
 
   constructor(props){
     super(props);
@@ -25,10 +16,6 @@ export default class ShipSprite extends Component {
       loop: false,
       characterPosition: {x: 0, y: 0}
     };
-
-    this.handlePlayStateChanged = this.handlePlayStateChanged.bind(this);
-    this.checkKeys = this.checkKeys.bind(this);
-    this.update = this.update.bind(this);
   }
 
   componentDidMount(){
@@ -37,15 +24,6 @@ export default class ShipSprite extends Component {
   componentWillUnmount() {
   }
 
-  getWrapperStyles() {
-    const { scale } = this.context;
-    const style = this.state.characterPosition.y;
-    return {
-      position: 'absolute',
-      transform: `translate({this.state.characterPosition.x * scale}px, {this.state.characterPosition.y * scale}px)`,
-      transformOrigin: 'left top',
-    };
-  }
 
   render() {
     const upDown = this.props.upArrow;
@@ -56,7 +34,7 @@ export default class ShipSprite extends Component {
           style = {{top: upDown, left: leftRight}}
           repeat={false}
           src={require('../Assets/Images/Sprites/BigBoatGB.png')}
-          scale={this.context.scale * 1}
+          scale={.50}
           state={0}
           steps={[0]}
           tileHeight={200}
@@ -65,54 +43,4 @@ export default class ShipSprite extends Component {
       </View>
     );
   }
-
-  handlePlayStateChanged(state) {
-    this.setState({
-      spritePlaying: state ? true : false,
-    });
-  };
-
-  move(body, x) {
-    Matter.Body.setVelocity(body, { x, y: 0});
-  };
-
-  checkKeys(shouldMoveStageLeft, shouldMoveStageRight) {
-    const { keys, store } = this.props;
-    const { body } = this.body;
-
-    let characterState = 0;
-
-    if(keys.isDown(keys.LEFT)) {
-      if(shouldMoveStageLeft) {
-        store.setStageX(store.stageX + 5);
-      }
-
-      this.move(body, -5);
-      characterState = 0;
-    } else if(keys.isDown(keys.RIGHT)) {
-      if(shouldMoveStageRight) {
-        store.setStageX(store.stageX - 5);
-      }
-
-      this.move(body, 5);
-      characterState = 0;
-    }
-
-    this.setState({
-      characterState,
-      repeat: characterState < 2,
-    });
-  };
-
-  update() {
-    const { store } = this.props;
-    const { body } = this.body;
-
-    const midPoint = math.abs(store.stageX) + 250;
-
-    const shouldMoveStageLeft = body.position.x < midPoint && store.stageX < 0;
-    const shouldMoveStageRight = body.position.x > midPoint && store.stageX > -576;
-
-    this.lastX = body.position.x;
-  };
 }//end of class
--- Component/GamePage.js
+++ Component/GamePage.js
@@ -11,29 +11,30 @@ export default class GamePage extends React.Component {
   super(props);
   this.state = {
     verticalMove: 0,
-    horizontalMove: 0
+    horizontalMove: 0,
+    playerMove: 25
   }
 }
 
 upPress = () => {
   this.setState({
-    verticalMove: this.state.verticalMove - 50
+    verticalMove: this.state.verticalMove - this.state.playerMove
   });
 }
 
 downPress = () => {
   this.setState({
-    verticalMove: this.state.verticalMove + 50
+    verticalMove: this.state.verticalMove + this.state.playerMove
   });
 }
 leftPress = () => {
   this.setState({
-    horizontalMove: this.state.horizontalMove - 50
+    horizontalMove: this.state.horizontalMove - this.state.playerMove
   });
 }
 rightPress = () => {
   this.setState({
-    horizontalMove: this.state.horizontalMove + 50
+    horizontalMove: this.state.horizontalMove + this.state.playerMove
   });
 }
 
@@ -44,16 +45,16 @@ rightPress = () => {
         <Stage width={576} height={1024}>
         <World>
             <WaterTileMap />
-              <TouchableOpacity style={{top: -325, left: 175, zIndex: 10}} onPress= {this.upPress}>
+              <TouchableOpacity style={{top: -325, left: "44%", zIndex: 10, width: 50, height:50}} onPress= {this.upPress}>
                 <Image source={require("./Assets/Images/Arrow.png")} style={{width: 50, height: 50}} />
               </TouchableOpacity>
-              <TouchableOpacity style={{top: 300, left: 175, zIndex: 10}} onPress= {this.downPress}>
+              <TouchableOpacity style={{top: 300, left: "44%", zIndex: 10, width: 50, height:50}} onPress= {this.downPress}>
                 <Image source={require("./Assets/Images/Arrow.png")} style={{width: 50, height: 50, transform: [{rotate: "180deg"}]}} />
               </TouchableOpacity>
-              <TouchableOpacity style={{top: -100, left: 370, zIndex: 10}} onPress= {this.rightPress}>
+              <TouchableOpacity style={{top: "-10%", left: 370, zIndex: 10, width: 50, height:50}} onPress= {this.rightPress}>
                 <Image source={require("./Assets/Images/Arrow.png")} style={{width: 50, height: 50, transform: [{rotate: "90deg"}]}} />
               </TouchableOpacity>
-              <TouchableOpacity style={{top: -150, left: 0, zIndex: 9}} onPress= {this.leftPress}>
+              <TouchableOpacity style={{top: "-17%", left: 0, zIndex: 10, width: 50, height:50}} onPress= {this.leftPress}>
                 <Image source={require("./Assets/Images/Arrow.png")} style={{width: 50, height: 50, transform: [{rotate: "270deg"}]}} />
               </TouchableOpacity>
               <ShipSprite upArrow={this.state.verticalMove} leftArrow={this.state.horizontalMove} />
