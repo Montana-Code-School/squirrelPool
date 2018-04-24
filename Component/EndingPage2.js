8374899514d80de759655cd16ebb074bb67307fa
Eli Dery <elidery@Ye-Olde-Macbook-Pro.local>
2018-04-24 15:52:28 -0600

    Added fonts and styling to all the pages

--- Component/EndingPage2.js
+++ Component/EndingPage2.js
@@ -2,28 +2,74 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { TouchableHighlight, Image, StyleSheet, Text, View } from 'react-native';
 import { StackNavigator } from 'react-navigation';
+import { Font } from 'expo';
 
 export default class EndingPage2 extends React.Component {
 
 constructor(props) {
   super(props);
+  this.state = {
+    fontLoaded: false
+  }
 }
+
+async componentDidMount() {
+  await Font.loadAsync({
+    'TradeWinds-Regular': require('./Assets/TradeWinds-Regular.ttf')
+  });
+  this.setState({
+    fontLoaded: true
+  });
+}
+
 landPageNav(e){
   this.props.navigation.navigate('LandingPage');
 }
 
   render () {
     return (
-      <View>
-      <Text>Arrr! Captn' Redtail Wins!</Text>
+      <View style={endingPage2Style.main}>
+      {
+        this.state.fontLoaded ? (
+          <Text style={endingPage2Style.win}>
+          Arrr! Captn' Redtail Wins!
+          </Text>
+        ) : null
+      }
         <TouchableHighlight
-          onPress={(e) => this.landPageNav(e)}>
+          onPress={(e) => this.landPageNav(e)}
+          style={endingPage2Style.touch}>
         <Image
           source={require("./Assets/Images/SQUIRRELWIN2.png")}
-          style={{width: '100%', height: '100%'}}
+          style={endingPage2Style.image}
         />
         </TouchableHighlight>
       </View>
     );
   }
 }
+const endingPage2Style = StyleSheet.create({
+  main: {
+    flex: 1,
+    backgroundColor: 'rgb(82, 224, 0)',
+    zIndex: 1,
+  },
+  image: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+    zIndex: 10,
+    marginTop: -150
+  },
+  win: {
+    fontSize: 50,
+    paddingBottom: -500,
+    zIndex: 5,
+    textAlign: 'center',
+    paddingTop: 20,
+    fontFamily: 'TradeWinds-Regular'
+  },
+  touch: {
+    flex: 1,
+  }
+})
--- Component/Instruction.js
+++ Component/Instruction.js
@@ -2,7 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { TouchableWithoutFeedback, Image, StyleSheet, Text, View } from 'react-native';
 import { StackNavigator } from 'react-navigation';
-import GamePage from './GamePage.js'
+import GamePage from './GamePage.js';
+import { Font } from 'expo';
 
 export default class InstructionPage extends React.Component {
 
@@ -10,7 +11,16 @@ constructor(props) {
   super(props);
   this.state = {
     press: 0,
+    fontLoaded: false,
   }
+}
+async componentDidMount() {
+  await Font.loadAsync({
+    'TradeWinds-Regular': require('./Assets/TradeWinds-Regular.ttf')
+  });
+  this.setState({
+    fontLoaded: true
+  });
 }
 
 gamePageNav(e){
@@ -24,31 +34,63 @@ gamePageNav(e){
 
   render () {
     return (
-      <View style={landingPageStyle.container}>
-
-          <Text style={{width: '60%', fontSize: 25}}>
+      <View style={instructionPageStyle.container}>
+      {
+        this.state.fontLoaded ? (
+        <View style={instructionPageStyle.container}>
+          <Text style = {instructionPageStyle.title}>
             How t' Play:
-            {"\n"}{"\n"}{"\n"}
+            </Text>
+            <Text style={instructionPageStyle.main}>
             Players alternate turns movin' thar color o' galleon across th' map usin'
             th' arrow buttons. Th' whirlpools will cause ye t' lose a turn. Th' first
             player t' leave th' island wit' th' loot be th' winner!
-            {"\n"}{"\n"}
             </Text>
-            <TouchableWithoutFeedback onPress={(e) => this.gamePageNav(e)}>
-          <Text>
+            <TouchableWithoutFeedback style={instructionPageStyle.touchable}
+              onPress={(e) => this.gamePageNav(e)}>
+          <Text style={instructionPageStyle.touchable}>
             Tap here to play
           </Text>
         </TouchableWithoutFeedback>
+      </View>
+        ) : null
+      }
       </View>
     );
   }
 }
 
-const landingPageStyle = StyleSheet.create({
+const instructionPageStyle = StyleSheet.create({
   container: {
     backgroundColor: '#a8ecff',
     flex: 1,
     alignItems: 'center',
     justifyContent: 'space-around',
+
   },
+    title: {
+      flex: 2,
+      alignItems: 'center',
+      fontWeight: 'bold',
+      marginTop: 100,
+      fontSize: 35,
+      fontFamily: 'TradeWinds-Regular'
+    },
+    main: {
+      flex: 2,
+      alignItems: 'center',
+      textAlign: 'center',
+      height: '100%',
+      fontSize: 20,
+      marginTop: -100,
+      paddingLeft: 20,
+      paddingRight: 20,
+      fontFamily: 'TradeWinds-Regular'
+    },
+    touchable: {
+      flex: 1,
+      marginTop: 75,
+      fontSize: 20,
+      fontFamily: 'TradeWinds-Regular'
+    }
 });
